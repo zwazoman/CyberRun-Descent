d23fbda667d86001698d4a9af89f0a893162e4c4
zwazoman <141416442+zwazoman@users.noreply.github.com>
2024-09-13 18:07:34 +0200

    un

---
+++ CyberRun-Descent/Assets/_Scripts/PlayerControls.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    int con = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        con += 5;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
