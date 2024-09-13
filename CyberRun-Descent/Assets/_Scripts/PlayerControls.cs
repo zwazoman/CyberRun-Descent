38b5881855db6ac0c5342f2dd0e878f6b0e8ee69
zwazoman <141416442+zwazoman@users.noreply.github.com>
2024-09-13 19:45:07 +0200

    dogs

--- CyberRun-Descent/Assets/_Scripts/PlayerControls.cs
+++ CyberRun-Descent/Assets/_Scripts/PlayerControls.cs
@@ -1,20 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    int con = 5;
-    // Start is called before the first frame update
-    void Start()
+    public bool IsGrounded { get; set; }
+    public bool IsDiving { get; private set; }
+
+    [SerializeField] float _jumpForce = 1;
+    [SerializeField] float _floatForce = 1;
+    [SerializeField] float _diveForce = 1;
+
+    bool _isJumping;
+    bool _stopFloating = false;
+    Rigidbody _rb;
+    
+
+    private static PlayerControls instance = null;
+    public static PlayerControls Instance => instance;
+    private void Awake()
     {
-        con += 5;
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (_isJumping) 
+        {
+            _rb.AddForce(Vector3.up * _floatForce);
+        }
+    }
 
+    public void OnSpaceBar(InputAction.CallbackContext context)
+    {
+        if (context.performed) 
+        {
+            if (IsGrounded)
+            {
+                print("Jump");
+                Jump();
+                _isJumping = true; 
+            }
+            else
+            {
+                print("Dive");
+                Dive();
+            }
+        }
+        if (context.canceled)
+        {
+            _isJumping = false;
+        }
+    }
+
+    void Jump()
+    {
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+    
+    void Dive()
+    {
+        _rb.AddForce(Vector3.down * _diveForce);
+        IsDiving = true;
     }
 }
