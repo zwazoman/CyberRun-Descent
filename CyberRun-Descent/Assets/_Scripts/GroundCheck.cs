bd487eb7d6ab1664ccbe674ab7b3b703ffa47871
Nathan <63930671+nathan-exe@users.noreply.github.com>
2024-09-15 21:01:57 +0200

    rework collisions

--- CyberRun-Descent/Assets/_Scripts/GroundCheck.cs
+++ CyberRun-Descent/Assets/_Scripts/GroundCheck.cs
@@ -13,12 +13,46 @@ public class GroundCheck : MonoBehaviour
 
     Collider _coll;
 
+    bool isGrounded = false;
+
     private void Awake()
     {
         _coll = gameObject.GetComponent<Collider>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
+    {
+        GameManager.instance.OnGameOver += () => enabled = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.SphereCast(Player.Instance.RB.position+ Vector3.up*.5f, .5f, Vector3.down, out RaycastHit hit,Mathf.Max(-Player.Instance.RB.velocity.y*Time.deltaTime,.6f), LayerMask.GetMask("Limits")))
+        {
+            print("grounded ptn");
+            if (!isGrounded)
+            {
+
+                if (Player.Instance.IsDiving)
+                {
+                    OnGroundDiveHit?.Invoke();
+                }
+                else
+                {
+                    OnGroundHit?.Invoke();
+                }
+                isGrounded = true;
+            }
+
+        }
+        else if(isGrounded)
+        {
+            isGrounded = false;
+            OnGroundLeave?.Invoke();
+        }
+    }
+
+   /* private void OnTriggerEnter(Collider other)
     {
         if (Player.Instance.IsDiving)
         {
@@ -28,10 +62,10 @@ public class GroundCheck : MonoBehaviour
         {
             if (other.gameObject.layer == 6) OnGroundHit?.Invoke();
         }
-    }
+    }*/
 
-    private void OnTriggerExit(Collider other)
+   /* private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.layer == 6) OnGroundLeave?.Invoke();
-    }
+    }*/
 }
--- CyberRun-Descent/Assets/_Scripts/Platforms.cs
+++ CyberRun-Descent/Assets/_Scripts/Platforms.cs
@@ -6,8 +6,14 @@ public class Platforms : MonoBehaviour
 {
     [SerializeField] float _moveSpeed;
 
+    private void Start()
+    {
+        _moveSpeed *= GameManager.Difficulty;
+    }
+
     private void Update()
     {
         transform.position += Vector3.left * _moveSpeed * Time.deltaTime;
+        if(transform.position.x<-100)Destroy(gameObject);
     }
 }
