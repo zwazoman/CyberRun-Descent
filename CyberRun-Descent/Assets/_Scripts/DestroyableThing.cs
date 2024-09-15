ec965a740aa540c300d7ca3e81f723c3916b9644
Nathan <63930671+nathan-exe@users.noreply.github.com>
2024-09-15 22:48:51 +0200

    coin

--- CyberRun-Descent/Assets/_Scripts/DestroyableThing.cs
+++ CyberRun-Descent/Assets/_Scripts/DestroyableThing.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class DestroyableThing : MonoBehaviour
 {
+    [SerializeField]scorePopup scorePopupPrefab;
     public void hit()
     {
+        Vector3 center = GetComponent<Collider>().bounds.center;
+        scorePopup popup = GameObject.Instantiate(scorePopupPrefab, center, Quaternion.identity);
+        popup.init(100, Color.white);
+
         Destroy(gameObject);
     }
 }
