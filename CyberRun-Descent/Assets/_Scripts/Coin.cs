5445b932f464907af83d1cfc99fdc24a9fc11af9
Nathan <63930671+nathan-exe@users.noreply.github.com>
2024-09-15 23:07:16 +0200

    pieces je bande

--- CyberRun-Descent/Assets/_Scripts/Coin.cs
+++ CyberRun-Descent/Assets/_Scripts/Coin.cs
@@ -7,20 +7,26 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using UnityEngine.VFX;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] GameObject scorePopupPrefab;
+    [SerializeField] VisualEffect PickUpVFXprefab;
+    
     //private TMP_Text CoinTexte;
     bool isDead = false;
+
 
     [SerializeField] float magnetDistance = 3;
     GameObject player => Player.Instance.gameObject;
 
     Vector3 vel = new();
 
-
+    private void Start()
+    {
+        if (Random.value < 0.5f) Destroy(gameObject);
+    }
 
     private void Update()
     {
@@ -28,7 +34,7 @@ public class Coin : MonoBehaviour
         if (Vector3.SqrMagnitude(offset) < magnetDistance * magnetDistance )
         {
             magnetDistance = 100;
-            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref vel, 0.07f,10);
+            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref vel, 0.07f);
         }
 
         if (!isDead)
@@ -51,6 +57,7 @@ public class Coin : MonoBehaviour
         //AudioManager.Instance.PlayCoin();
         
         transform.localScale *= 1.6f;
+        Destroy(GameObject.Instantiate(PickUpVFXprefab, transform.position, Quaternion.identity),2);
 
         GameObject Popup = GameObject.Instantiate(scorePopupPrefab, transform.position + Vector3.forward * -1, Quaternion.identity);
         Popup.GetComponent<scorePopup>().init(200, Color.white);
